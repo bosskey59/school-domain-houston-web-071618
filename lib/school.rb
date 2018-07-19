bebdbf627c6d4005d2511579318e97ddd4cbca49
Alejandro Aguilar Jr <alejandro.aguilar.jr@gmail.com>
2018-07-19 00:38:51 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+require "pry"
+class School
+
+  def initialize(name)
+    @name=name
+    @roster={}
+  end
+
+  attr_accessor :roster
+
+  def add_student(name, grade)
+    if !@roster.has_key?(grade)
+      @roster[grade]=[]
+    end
+    # if !@roster[grade].include?(name)
+    @roster[grade]<<name
+  end
+
+  def grade(grade_num)
+    @roster.each do |key, value|
+      if key==grade_num
+        return value
+      end
+    end
+  end
+
+  def sort
+    @roster.each do |key, value|
+      @roster[key]=value.sort
+    end
+    @roster
+  end
+
+end
